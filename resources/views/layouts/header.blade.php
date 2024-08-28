4d5010717b9ad88c5fc35d9e2418e821117eeeb9
samirsignh <samirsingh8210@gmail.com>
2024-08-28 17:05:51 +0530

    updates

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -44,7 +44,8 @@
 
                                     </div>
                                     <div class="card-footer text-center p-3">
-                                        <a href="page-login.html" class="dropdown-item ai-icon btn btn-primary light">
+                                        <a href="{{ route('login') }}"
+                                            class="dropdown-item ai-icon btn btn-primary light">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                                                 viewBox="0 0 24 24" fill="none" stroke="var(--primary)" stroke-width="2"
                                                 stroke-linecap="round" stroke-linejoin="round">
